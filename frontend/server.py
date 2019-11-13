9c30fdf514c98b2a026a4e4d6702495c8da33e00
Malik <mjacks3@umbc.edu>
2019-11-12 22:48:25 -0500

    Restructured Front End

--- frontend/server.py
+++ frontend/server.py
@@ -11,5 +11,21 @@ def hello():
 def home():
     return render_template('index.html')
 
+@app.route("/index")
+def index():
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template('bout.html')
+
+@app.route("/existing")
+def existing():
+    return render_template('existing.html')
+
+@app.route("/new")
+def new():
+    return render_template('new.html')
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port= 1234)
